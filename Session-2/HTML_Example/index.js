4e752494ad6c249f91072b20044480211ec19e28
Himanshu Kotnala <himanshu.kotnala99@gmail.com>
2020-04-11 13:55:57 +0530

    Content Added

--- Session-2/HTML_Example/index.js
+++ Session-2/HTML_Example/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static('./statics'));
-app.use(bodyParser.urlencoded({ extended: false}));
+//app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/',(req,res) => {
-    res.sendFile('index.html',{root: __dirname+'/templates'})
+    res.sendFile('index.html',{root: __dirname+'/templates'});
 });
 
 app.listen(8080);
